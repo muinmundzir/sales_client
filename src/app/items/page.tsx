85db51667d60d08b7d674ccb000391be9e68432f
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-02 14:58:30 +0700

    feat: add create new item function

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -3,12 +3,15 @@
 import { Fragment, useCallback, useEffect, useState } from 'react'
 
 import Headers from '@app/components/Headers'
-import { formatDate } from '@app/helpers/format-date'
 import { formatCurrencyIDR } from '@app/helpers/format-currency'
 import { IItem } from '@app/interfaces/item.interface'
+import { PlusIcon } from '@heroicons/react/24/outline'
+import AddItemModal from './components/AddItemModal'
+import axios from 'axios'
 
 export default function Home() {
   const [items, setItems] = useState<IItem[]>([])
+  const [isOpen, setIsOpen] = useState(false)
 
   const fetchItem = useCallback(() => {
     let url = 'http://localhost:3000/items'
@@ -24,10 +27,35 @@ export default function Home() {
     fetchItem()
   }, [fetchItem])
 
+  const handleOnCloseModal = () => {
+    setIsOpen(false)
+  }
+
+  const handleDelete = async (itemId: number) => {
+    let url = `http://localhost:3000/items/${itemId}`
+    const response = await axios.delete(url)
+
+    if (response.status === 200) fetchItem()
+  }
+
   return (
     <Fragment>
       <Headers />
+      <AddItemModal
+        fetchData={fetchItem}
+        isOpen={isOpen}
+        onClose={handleOnCloseModal}
+      />
       <main className='mx-auto flex max-w-7xl justify-between p-6 lg:px-8 flex-col space-y-2'>
+        <div className='flex justify-end mb-3'>
+          <button
+            onClick={() => setIsOpen(true)}
+            className='flex items-center space-x-4 py-2 px-4 border border-blue-500 rounded-lg text-white bg-blue-500 shadow-md hover:bg-blue-400 hover:border-blue-400'
+          >
+            <PlusIcon className='h-5 w-5' />
+            Add item
+          </button>
+        </div>
         <table className='shadow-lg bg-white rounded-lg table-fixed'>
           <thead>
             <tr className='mx-10 text-center bg-gray-300/80'>
@@ -35,6 +63,7 @@ export default function Home() {
               <th className='py-3 px-1'>Kode</th>
               <th className='py-3 px-1'>Nama</th>
               <th className='py-3 px-1'>Harga</th>
+              <th className='py-3 px-1'>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -45,10 +74,28 @@ export default function Home() {
                     <td className='py-3 px-1'>{index + 1}</td>
                     <td className='py-3 px-1'>{item.code}</td>
                     <td className='py-3 px-1'>{item.name}</td>
-                    <td className='py-3 px-1'>{formatCurrencyIDR(item.price.toString())}</td>
+                    <td className='py-3 px-1'>
+                      {formatCurrencyIDR(item.price.toString())}
+                    </td>
+                    <td className='py-3 px-1'>
+                      <button
+                        type='button'
+                        onClick={() => handleDelete(item.id)}
+                        className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+                      >
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 )
               })}
+            {!items.length && (
+              <tr>
+                <td colSpan={10} className='py-3 px-1 text-center'>
+                  -- Data kosong --
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </main>
---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent } from "react"
+
+export default function NumberInput({
+  label,
+  name,
+  placeholder,
+  onChange,
+}: {
+  label: string
+  name: string
+  placeholder: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}) {
+  return (
+    <div>
+      <label
+        htmlFor={`${name}`}
+        className='block text-sm font-medium leading-6 text-gray-900'
+      >
+        {label}
+      </label>
+      <div className='relative mt-2 rounded-md shadow-sm'>
+        <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
+          <span className='text-gray-500 sm:text-sm'>Rp.</span>
+        </div>
+        <input
+          id={`${name}`}
+          name={`${name}`}
+          type='text'
+          placeholder={`${placeholder}`}
+          className='block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  )
+}
