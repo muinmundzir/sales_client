0dedc119c9cd216aabb45fb538ed2c9f85527dbe
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-06 06:26:28 +0700

    feat: update item interface

--- src/interfaces/item.interface.ts
+++ src/interfaces/item.interface.ts
@@ -1,11 +1,11 @@
-import { TransactionDetail } from "./transaction-detail.interface";
+import { ITransactionDetail } from './transaction-detail.interface'
 
 export interface IItem {
-  id: number;
-  code: string;
-  name: string;
-  price: number;
-  createdAt: Date;
-  updatedAt: Date;
-  saleDetail: TransactionDetail
+  id: number
+  code: string
+  name: string
+  price: number
+  createdAt: Date
+  updatedAt: Date
+  saleDetail: ITransactionDetail
 }
