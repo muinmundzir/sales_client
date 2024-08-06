464c90635d4b493458b59db6bc8c62e6f8ebdf12
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-07 00:27:04 +0700

    feat: modify navbar menu on mobile viewport

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -90,33 +90,26 @@ export default function Headers() {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
-                <Disclosure as='div' className='-mx-3'>
-                  <DisclosureButton className='group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                    Product
-                    <ChevronDownIcon
-                      aria-hidden='true'
-                      className='h-5 w-5 flex-none group-data-[open]:rotate-180'
-                    />
-                  </DisclosureButton>
-                </Disclosure>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                >
-                  Features
-                </a>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                >
-                  Marketplace
-                </a>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                >
-                  Company
-                </a>
+                <div className='flex flex-col gap-2'>
+                  <Link
+                    href='/'
+                    className='text-sm font-semibold leading-6 text-gray-900'
+                  >
+                    Transactions
+                  </Link>{' '}
+                  <Link
+                    href='/items'
+                    className='text-sm font-semibold leading-6 text-gray-900'
+                  >
+                    Items
+                  </Link>{' '}
+                  <Link
+                    href='/customers'
+                    className='text-sm font-semibold leading-6 text-gray-900'
+                  >
+                    Customers
+                  </Link>{' '}
+                </div>
               </div>
             </div>
           </div>
